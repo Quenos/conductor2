2fea0534725928b0cc3647582dc0d8859abd9b68
Quenos <coen.kuijpers@asild.eu>
2018-11-21 18:48:45 +0100

    Change classmethods to staticmethods

--- lightning/lndAL.py
+++ lightning/lndAL.py
@@ -21,169 +21,168 @@ from config.config import SystemConfiguration
 
 
 class LndAL(object):
+    _macaroon = None
+    _stub = None
 
     class LightningException(Exception):
         pass
 
-    def __init__(self):
-        pass
-
-    @classmethod
-    def get_rpc_data(cls):
+    @staticmethod
+    def get_rpc_data():
         system_config = SystemConfiguration()
-        cls.macaroon = codecs.encode(open(system_config.admin_macaroon_directory + '/admin.macaroon', 'rb').read(),
+        LndAL._macaroon = codecs.encode(open(system_config.admin_macaroon_directory + '/admin.macaroon', 'rb').read(),
                                      'hex')
         os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
         cert = open(system_config.tls_cert_directory + '/tls.cert', 'rb').read()
         ssl_creds = grpc.ssl_channel_credentials(cert)
         channel = grpc.secure_channel(system_config.lnd_rpc_address + ':' + system_config.lnd_rpc_port, ssl_creds)
-        cls.stub = lnrpc.LightningStub(channel)
+        LndAL._stub = lnrpc.LightningStub(channel)
 
-    @classmethod
-    def get_rpc_unlock_data(cls):
-        cls.macaroon = codecs.encode(open('/home/coen/data/admin.macaroon', 'rb').read(), 'hex')
+    @staticmethod
+    def get_rpc_unlock_data():
+        system_config = SystemConfiguration()
+        LndAL._macaroon = codecs.encode(open(system_config.admin_macaroon_directory + '/admin.macaroon', 'rb').read(),
+                                        'hex')
         os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
-        cert = open('/home/coen/data/tls.cert', 'rb').read()
+        cert = open(system_config.tls_cert_directory + '/tls.cert', 'rb').read()
         ssl_creds = grpc.ssl_channel_credentials(cert)
-        channel = grpc.secure_channel('192.168.0.110:10009', ssl_creds)
-        cls.stub = lnrpc.WalletUnlockerStub(channel)
+        channel = grpc.secure_channel(system_config.lnd_rpc_address + ':' + system_config.lnd_rpc_port, ssl_creds)
+        LndAL._stub = lnrpc.WalletUnlockerStub(channel)
 
-    @classmethod
-    def describe_graph(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def describe_graph():
+        LndAL.get_rpc_data()
         request = ln.ChannelGraphRequest()
-        return cls.stub.DescribeGraph(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.DescribeGraph(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def channel_balance(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def channel_balance():
+        LndAL.get_rpc_data()
         request = ln.ChannelBalanceRequest()
-        return cls.stub.ChannelBalance(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.ChannelBalance(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def wallet_balance(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def wallet_balance():
+        LndAL.get_rpc_data()
         request = ln.WalletBalanceRequest()
-        return cls.stub.WalletBalance(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.WalletBalance(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def list_channels(cls, active_only=False, inactive_only=False, public_only=False, private_only=False):
-        cls.get_rpc_data()
+    @staticmethod
+    def list_channels(active_only=False, inactive_only=False, public_only=False, private_only=False):
+        LndAL.get_rpc_data()
         request = ln.ListChannelsRequest(
             active_only=active_only,
             inactive_only=inactive_only,
             public_only=public_only,
             private_only=private_only)
-        return cls.stub.ListChannels(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.ListChannels(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def closed_channels(cls,
-                        cooperative=False,
+    @staticmethod
+    def closed_channels(cooperative=False,
                         local_force=False,
                         remote_force=False,
                         breach=False,
                         funding_canceled=False):
-        cls.get_rpc_data()
+        LndAL.get_rpc_data()
         request = ln.ClosedChannelsRequest(cooperative=cooperative,
                                            local_force=local_force,
                                            remote_force=remote_force,
                                            breach=breach,
                                            funding_canceled=funding_canceled)
-        return cls.stub.ClosedChannels(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.ClosedChannels(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def get_transactions(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def get_transactions():
+        LndAL.get_rpc_data()
         request = ln.GetTransactionsRequest()
-        return cls.stub.GetTransactions(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.GetTransactions(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def get_info(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def get_info():
+        LndAL.get_rpc_data()
         request = ln.GetInfoRequest()
-        return cls.stub.GetInfo(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.GetInfo(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def forwarding_history(cls, start_time=0, end_time=0, index_offset=0, num_max_events=0):
-        cls.get_rpc_data()
+    @staticmethod
+    def forwarding_history(start_time=0, end_time=0, index_offset=0, num_max_events=0):
+        LndAL.get_rpc_data()
         request = ln.ForwardingHistoryRequest(
             start_time=start_time,
             end_time=end_time,
             index_offset=index_offset,
             num_max_events=num_max_events)
-        return cls.stub.ForwardingHistory(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.ForwardingHistory(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def query_routes(cls, pub_key, fee_limit, amt=0, num_routes=10, final_cltv_delta=0 ):
-        cls.get_rpc_data()
+    @staticmethod
+    def query_routes(pub_key, fee_limit, amt=0, num_routes=10, final_cltv_delta=0):
+        LndAL.get_rpc_data()
         request = ln.QueryRoutesRequest(
             pub_key=pub_key,
             amt=amt,
             num_routes=num_routes,
             fee_limit=fee_limit,
             final_cltv_delta=final_cltv_delta)
-        return cls.stub.QueryRoutes(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.QueryRoutes(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def decode_pay_req(cls, pay_req):
-        cls.get_rpc_data()
+    @staticmethod
+    def decode_pay_req(pay_req):
+        LndAL.get_rpc_data()
         request = ln.PayReqString(pay_req=pay_req)
-        return cls.stub.DecodePayReq(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.DecodePayReq(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def new_address(cls, type):
-        cls.get_rpc_data()
+    @staticmethod
+    def new_address(type):
+        LndAL.get_rpc_data()
         request = ln.NewAddressRequest(type=type)
-        return cls.stub.NewAddress(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.NewAddress(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def unlock_wallet(cls, wallet_password, recovery_window=0):
-        cls.get_rpc_unlock_data()
+    @staticmethod
+    def unlock_wallet(wallet_password, recovery_window=0):
+        LndAL.get_rpc_unlock_data()
         request = ln.UnlockWalletRequest(wallet_password=wallet_password, recovery_window=recovery_window)
-        return cls.stub.UnlockWallet(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.UnlockWallet(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def update_channel_policy(cls, chan_point, base_fee_msat, fee_rate, time_lock_delta ):
-        cls.get_rpc_data()
+    @staticmethod
+    def update_channel_policy(chan_point, base_fee_msat, fee_rate, time_lock_delta):
+        LndAL.get_rpc_data()
         request = ln.PolicyUpdateRequest(chan_point=chan_point,
                                          base_fee_msat=base_fee_msat,
                                          fee_rate=fee_rate,
                                          time_lock_delta=time_lock_delta)
-        return cls.stub.UpdateChannelPolicy(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.UpdateChannelPolicy(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def get_node_info(cls, pub_key):
-        cls.get_rpc_data()
+    @staticmethod
+    def get_node_info(pub_key):
+        LndAL.get_rpc_data()
         request = ln.NodeInfoRequest(pub_key=pub_key)
-        return cls.stub.GetNodeInfo(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.GetNodeInfo(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def get_chan_info(cls, chan_id):
-        cls.get_rpc_data()
+    @staticmethod
+    def get_chan_info(chan_id):
+        LndAL.get_rpc_data()
         request = ln.ChanInfoRequest(chan_id=chan_id)
-        return cls.stub.GetChanInfo(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._macaroon.GetChanInfo(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def get_fee_report(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def get_fee_report():
+        LndAL.get_rpc_data()
         request = ln.FeeReportRequest()
-        return cls.stub.FeeReport(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.FeeReport(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def pending_channels(cls):
-        cls.get_rpc_data()
+    @staticmethod
+    def pending_channels():
+        LndAL.get_rpc_data()
         request = ln.PendingChannelsRequest()
-        return cls.stub.PendingChannels(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.PendingChannels(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def connect(cls, addr, perm=False):
-        cls.get_rpc_data()
+    @staticmethod
+    def connect(addr, perm=False):
+        LndAL.get_rpc_data()
         request = ln.ConnectPeerRequest(addr=addr)
-        return cls.stub.ConnectPeer(request, metadata=[('macaroon', cls.macaroon)])
+        return LndAL._stub.ConnectPeer(request, metadata=[('macaroon', LndAL._macaroon)])
 
-    @classmethod
-    def open_channel(cls,
-                     node_pub_key,
+    @staticmethod
+    def open_channel(node_pub_key,
                      node_pub_key_string,
                      local_funding_amount,
                      push_sat=0,
@@ -195,7 +194,7 @@ class LndAL(object):
                      min_confs=1,
                      spend_unconfirmed=False):
         response = []
-        cls.get_rpc_data()
+        LndAL.get_rpc_data()
         request = ln.OpenChannelRequest(node_pubkey=node_pub_key,
                                         node_pubkey_string=node_pub_key_string,
                                         local_funding_amount=local_funding_amount,
@@ -206,22 +205,22 @@ class LndAL(object):
                                         min_htlc_msat=min_htlc_msat,
                                         remote_csv_delay=remote_csv_delay)
 
-        for r in cls.stub.OpenChannel(request, metadata=[('macaroon', cls.macaroon)]):
+        for r in LndAL._stub.OpenChannel(request, metadata=[('macaroon', LndAL._macaroon)]):
             # TODO: test the below
             response.append(r)
             if r:
                 break
         return response
 
-    @classmethod
-    def close_channel(cls, channel_point, force=False, target_conf=0, sat_per_byte=0):
+    @staticmethod
+    def close_channel(channel_point, force=False, target_conf=0, sat_per_byte=0):
         response = []
-        cls.get_rpc_data()
+        LndAL.get_rpc_data()
         request = ln.CloseChannelRequest(channel_point=channel_point,
                                          force=force,
                                          target_conf=target_conf,
                                          sat_per_byte=sat_per_byte)
-        for r in cls.stub.CloseChannel(request, metadata=[('macaroon', cls.macaroon)]):
+        for r in LndAL._stub.CloseChannel(request, metadata=[('macaroon', LndAL._macaroon)]):
             # TODO: test the below
             response.append(r)
             if r:
